17c8ad2a718d799d1a21c251d23356b7c3d3e6d4
syr0x <guardi4n0x00@gmail.com>
2018-01-05 16:48:22 -0200

    update

--- ip2loc.py
+++ ip2loc.py
@@ -2,21 +2,47 @@ import requests
 from bs4 import BeautifulSoup
 import sys
 
-if len(sys.argv) != 1:
+if len(sys.argv) != 2:
     print('Usage:' + sys.argv[0] + '<country>')
     sys.exit(1)
 
 country = sys.argv[1]
 
-page = requests.get('https://lite.ip2location.com/ip-address-ranges-by-country')
+def parsePage(link):
+    # Create a BeautifulSoup object
+    page = requests.get(link)
+    soup = BeautifulSoup(page.text, 'html.parser')
+    return soup
 
-# Create a BeautifulSoup object
-soup = BeautifulSoup(page.text, 'html.parser')
-countryList = soup.find(class_='content-page')
-countryName = countryList.find_all('li')
 
-for country in countryName:
-    names = country.contents[2]
-    links = 'https://lite.ip2location.com/' + str(country.get('href'))
-    print(names)
-    print(links)
+# def runMasscan(iprange):
+#     print("masscan --range "+ ip1 +" - ")
+
+
+
+def getLines(soupObj):
+    tableContent = soupObj.find("table")
+    for td in tableContent.findAll('tr'):
+        key = td.get_text()
+        print(key)
+    return
+
+
+
+def countryLink():
+    link = ('https://lite.ip2location.com//'+ str(country).lower() + '-ip-address-ranges')
+    print('[+] Opened '+ country)
+    print('-' * 40)
+    print('[+] STARTING CRAWLING')
+    return link
+
+
+
+if __name__ == "__main__":
+    print('\n[+] STARTING')
+    link = countryLink()
+    soupObj = parsePage(link)
+    getLines(soupObj)
+
+
+
