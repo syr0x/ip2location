1e180b57c440ceb85d9d7237a1f00736432bdec8
syr0x <guardi4n0x00@gmail.com>
2018-01-09 15:21:17 -0200

    update

--- ip2loc.py
+++ ip2loc.py
@@ -2,7 +2,10 @@ import requests
 from bs4 import BeautifulSoup
 import sys
 import masscan
+import subprocess
 import pycountry as pc
+from multiprocessing import Process, Queue
+
 
 if len(sys.argv) != 3:
     print('Usage:' + sys.argv[0] + '<country>' + ' <ipverse || ip2location>')
@@ -11,6 +14,46 @@ if len(sys.argv) != 3:
 country = sys.argv[1]
 source = sys.argv[2]
 
+def worker(input, output):
+    print(input())
+    print(output)
+
+
+def runMasscan(ipranges):
+    print('[+] STARTING MASSCAN')
+    ipranges = ipranges.split()
+    processNum = 5
+    massTask = 'masscan --range'
+
+    # Create queues
+    taskQueue = Queue()
+    doneQueue = Queue()
+
+    # Submit tasks
+    for task in massTask:
+        taskQueue.put(task)
+
+    # Start worker processes
+    for i in range(processNum):
+        Process(target=worker, args=(task_queue, done_queue)).start()
+
+    # Get and print results
+
+def getLines(soupObj):
+    ipRanges = []
+    if source == 'ip2location':
+        tableContent = soupObj.find("table")
+        for td in tableContent.findChildren('tr'):
+            key = td.get_text()
+            ipRanges.append(key)
+    else:
+        content = soupObj.get_text()
+        for s in content.splitlines():
+            if s.strip().startswith('#'):
+                pass
+            else:
+                ipRanges.append(s.strip())
+    return ipRanges
 
 def parsePage(link):
     # Create a BeautifulSoup object
@@ -19,26 +62,15 @@ def parsePage(link):
     return soup
 
 
-# def runMasscan(iprange):
-#     masscan.
-#     print("masscan --range "+ ip1 +" - ")
-
-
-def getLines(soupObj):
-    tableContent = soupObj.find("table")
-    for td in tableContent.findChildren('tr'):
-        key = td.get_text()
-#        runMasscan(key)
-    return
-
-
 def countryLink(source):
     if source.lower() == 'ip2location':
         link = ('https://lite.ip2location.com//' + str(country).lower() + '-ip-address-ranges')
+        print('[+] Ip2Location setup')
     else:
         code = pc.countries.get(name=country.title()).alpha_2
         link = ('http://ipverse.net/ipblocks/data/countries/'+ str(code).lower() + '.zone')
-    print('[+] Opened ' + country)
+        print('[+] IpVerse setup')
+    print('[+] Opened ' + country.title())
     print('-' * 40)
     print('[+] STARTING CRAWLING')
     return link
@@ -48,4 +80,5 @@ if __name__ == "__main__":
     print('\n[+] STARTING')
     link = countryLink(source)
     soupObj = parsePage(link)
-    getLines(soupObj)
+    ipRanges = getLines(soupObj)
+    runMasscan(ipRanges)
