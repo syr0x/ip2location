296239dd98f4b5c5dd57fbe7fe5a34a1923e6cef
syr0x <guardi4n0x00@gmail.com>
2018-01-12 16:11:30 -0200

    update ip2loc.py

--- ip2loc.py
+++ ip2loc.py
@@ -1,59 +1,106 @@
 import requests
 from bs4 import BeautifulSoup
 import sys
-import masscan
 import subprocess
 import pycountry as pc
-from multiprocessing import Process, Queue
+import multiprocessing
+import time
 
 
-if len(sys.argv) != 3:
-    print('Usage:' + sys.argv[0] + '<country>' + ' <ipverse || ip2location>')
+if len(sys.argv) != 4:
+    print('Usage:' + sys.argv[0] + ' <country>' + ' <ipverse||ip2location> ' + '<process>')
     sys.exit(1)
 
+# set args
 country = sys.argv[1]
 source = sys.argv[2]
+processNum = sys.argv[3]
 
-def worker(input, output):
-    print(input())
+def runMasscan(iprange):
+    savedFile = '//Users//jonathan//shodanwave//masscan//' + str(iprange)+'.txt'
+    process = subprocess.Popen(["masscan","-p22", str(iprange), "--rate=4000", '-oG', savedFile], stdout=subprocess.PIPE)
+    output, error = process.communicate()
+    status = process.wait()
     print(output)
+    return status, output
 
 
-def runMasscan(ipranges):
-    print('[+] STARTING MASSCAN')
-    ipranges = ipranges.split()
-    processNum = 5
-    massTask = 'masscan --range'
+def worker(work, function, return_dict):
+    while not work.empty():
+        try:
+            candidate = work.get()
+        except:
+            continue
+        if candidate:
+            ret = function(candidate)
+            if ret:
+                return_dict[candidate] = ret
 
-    # Create queues
-    taskQueue = Queue()
-    doneQueue = Queue()
 
-    # Submit tasks
-    for task in massTask:
-        taskQueue.put(task)
+class Multiprocessing:
+    @staticmethod
+    def main(workers, work_set, my_function):
+        manager = multiprocessing.Manager()
+        return_dict = manager.dict()
+        work = manager.Queue()
 
-    # Start worker processes
-    for i in range(processNum):
-        Process(target=worker, args=(task_queue, done_queue)).start()
+        for w in work_set:
+            work.put(w)
 
-    # Get and print results
+        processes = []
+        for unused_index in range(workers):
+            process = multiprocessing.Process(target=worker, args=(work, my_function, return_dict,))
+            processes.append(process)
+            process.start()
+
+        while not work.empty():
+            #print(work.qsize())
+            time.sleep(0.1)
+            continue
+
+
+        alive_process = True
+        while alive_process:
+            for process in processes:
+                process.terminate()
+                if process.is_alive():
+                    alive_process = True
+                    continue
+                alive_process = False
+        return return_dict
+
 
 def getLines(soupObj):
     ipRanges = []
-    if source == 'ip2location':
-        tableContent = soupObj.find("table")
-        for td in tableContent.findChildren('tr'):
-            key = td.get_text()
-            ipRanges.append(key)
-    else:
-        content = soupObj.get_text()
-        for s in content.splitlines():
-            if s.strip().startswith('#'):
-                pass
-            else:
-                ipRanges.append(s.strip())
-    return ipRanges
+    try:
+        if source == 'ip2location':
+            tableContent = soupObj.find("table")
+            for td in tableContent.findChildren('tr'):
+                key = td.get_text().split('\n')
+                if key[1] == "Begin IP Address":
+                    continue
+                elif int(key[3].replace(',', '')) > 256:
+                    key = (key[1] + "-" + key[2])
+                    ipRanges.append(key)
+
+
+        # for ip in ipRanges:
+        #     ip = ip.replace("\n", " ")
+        #     print(ip)
+
+        else:
+            content = soupObj.get_text()
+            for s in content.splitlines():
+                if s.strip().startswith('#'):
+                    pass
+                else:
+                    ipRanges.append(s.strip())
+
+        return ipRanges
+
+    except Exception as inst:
+        print(inst)
+
 
 def parsePage(link):
     # Create a BeautifulSoup object
@@ -81,4 +128,10 @@ if __name__ == "__main__":
     link = countryLink(source)
     soupObj = parsePage(link)
     ipRanges = getLines(soupObj)
-    runMasscan(ipRanges)
+    ret = Multiprocessing.main(int(processNum), ipRanges, runMasscan)
+
+
+
+
+
+
